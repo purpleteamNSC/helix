17dec3423462c025ec9fdf904f2f893282312495
Purple Team <118991961+purpleteamNSC@users.noreply.github.com>
2025-08-11 20:20:43 +0000

    trying to find v2 alerts from the web console

--- src/app.py
+++ src/app.py
@@ -12,6 +12,16 @@ APIKEY = os.getenv("APIKEY")
 
 # EXECUTION
 fireeye_tennat = hf(HELIX_ID, APIKEY)
-trellix_tennat = ht()
 
-print(trellix_tennat)
+# v1 ------- 
+# alertas_keys = fireeye_tennat.teste().keys()
+# alertas= fireeye_tennat.teste().get('alerts',[])
+# alerta= fireeye_tennat.teste().get('alerts',[])[0]
+
+# pprint(alerta)
+
+# v2 -------
+alertas_keys = fireeye_tennat.teste().keys()
+alertas = fireeye_tennat.teste().get('results',[])
+alerta = fireeye_tennat.teste().get('results',[])[0]
+pprint(alerta)
--- src/fireeye.py
+++ src/fireeye.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 class Helix:
@@ -11,7 +12,7 @@ class Helix:
         }
         self.base_url = f"https://apps.fireeye.com/helix/id/{self.helix_id}/api"
 
-
+    # PEGA TODAS AS QUERYS EXECUTADAS NA CONSOLE
     def get_archives(self):
         """
         _url web :
@@ -25,7 +26,7 @@ class Helix:
         except Exception as e:
             print(e)
 
-
+    # DELETA UMA PESQUISA ESPECIFICA
     def del_search(self, id):
         resource = f"{self.base_url}/v1/search/archive/{id}"
         try:
@@ -35,7 +36,7 @@ class Helix:
         except Exception as e:
             return "Erro ao buscar as pesquisas executadas"
 
-
+    # DELETA TODAS AS QUERY
     def del_search_all(self):
         resource = f"{self.base_url}/v1/search/archive/"
         try:
@@ -45,3 +46,48 @@ class Helix:
             return res.json()
         except:
             return "Error ao tentar excluir todas as pesquisas executadas"
+
+    ## ALERTAS
+
+    # PEGA TODOS OS ALERTAS NA VERSAO LEGACY
+    def get_alerts_v1(self):
+        resource = f"{self.base_url}/v1/alerts"
+
+        query_filter = {"state": {"$in": ["Open"]}}
+
+        query_str = json.dumps(query_filter)
+
+        params = {"query": query_str}
+
+        try:
+            res = requests.get(url=resource, headers=self.headers, params=params)
+            res.raise_for_status
+            return res.json()
+        except:
+            return "Error ao tentar buscar os alertas na versao 2"
+
+    def teste(self):
+        resource = f"{self.base_url}/v3/alerts/"
+
+        # query_filter = {
+        #     "state": {"$in": ["Open"]}
+        #     }
+
+        # query_str = json.dumps(query_filter)
+
+        # params = {
+        #     "query":query_str
+        # }
+        # ---------------------------------------------------
+        params = {
+            "state": "open", 
+            "order_by": "-createDate"
+            }
+
+        try:
+            res = requests.get(url=resource, headers=self.headers, params=params)
+            # res = requests.get(url=resource, headers=self.headers)
+            res.raise_for_status
+            return res.json()
+        except:
+            return "Error ao tentar buscar os alertas na versao 2"
